c461e262a9e4f60b4a7625ac994f971191559aca
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-27 21:49:56 +0200

    brouillard

--- backend/models/Things.js
+++ backend/models/Things.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema({
+const thingSchemaUser = mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
 });
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model('Thing', thingSchemaUser);
+
+const thingSchemaBook = mongoose.Schema({
+    userId: {type: String , required: true},
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    year: {type: Number, required: true},
+    livregenre: {type: String, required: true},
+    ratings : 
+        [ 
+            {   
+                userId: {type: String, required: true},     
+                grade: {type: Number, required: true},
+            }
+        ],
+    averageRating: {type: Number, required: true},
+});
+
+module.exports = mongoose.model('Thing', thingSchemaBook);
