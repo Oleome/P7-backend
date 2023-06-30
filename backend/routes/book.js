c273db251dcf692edcc0e66633eaf815178cb0dc
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-30 13:27:49 +0200

    auth

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', bookCtrl.createBook);
+router.post('/', auth, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/' , bookCtrl.getAllBooks);
 
 module.exports = router;
