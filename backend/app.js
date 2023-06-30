852d4b391cb0110b9741892b9d27df68f6ac7d4f
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-30 13:57:14 +0200

    wip

--- backend/app.js
+++ backend/app.js
@@ -265,5 +265,6 @@ app.use(bodyParser.json());
 
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
