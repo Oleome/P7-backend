14054e4f31ebf8627bcdc490ea4155ab3f113896
Pierre Pillot <pierrepillot0110@gmail.com>
2023-07-01 14:06:41 +0200

    routes définies

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -9,7 +9,8 @@ exports.createBook = (req, res, next) => {
     userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
-    book.save().then(
+  book.save()
+    .then(
       () => {
         res.status(201).json({
           message: 'Post saved successfully!'
@@ -22,24 +23,24 @@ exports.createBook = (req, res, next) => {
         });
       }
     );
-    next();
+  next();
 };
 
 exports.getOneBook = (req, res, next) => {
-    Book.findOne({
-      _id: req.params.id
-    }).then(
-      (book) => {
-        res.status(200).json(book);
-      }
-    ).catch(
-      (error) => {
+  Book.findOne({
+    _id: req.params.id
+  }).then(
+    (book) => {
+      res.status(200).json(book);
+    }
+  ).catch(
+    (error) => {
         res.status(404).json({
-          error: error
-        });
-      }
-    );
-    next();
+        error: error
+      });
+    }
+  );
+  next();
 };
 
 exports.modifyBook = (req, res, next) => {
@@ -50,19 +51,19 @@ exports.modifyBook = (req, res, next) => {
 
 delete bookObject._userId;
 Book.findOne({_id: req.params.id})
-    .then((book) => {
-        if (book.userId != req.auth.userId) {
-            res.status(401).json({ message : 'Not authorized'});
-        } else {
-            Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
-            .then(() => res.status(200).json({message : 'Objet modifié!'}))
-            .catch(error => res.status(401).json({ error }));
+  .then((book) => {
+    if (book.userId != req.auth.userId) {
+        res.status(401).json({ message : 'Not authorized'});
+    } else {
+        Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+          .then(() => res.status(200).json({message : 'Objet modifié!'}))
+          .catch(error => res.status(401).json({ error }));
         }
     })
-    .catch((error) => {
-        res.status(400).json({ error });
-    });
-    next();
+  .catch((error) => {
+      res.status(400).json({ error });
+  });
+  next();
 };
 
 exports.deleteBook = (req, res, next) => {
@@ -86,15 +87,25 @@ exports.deleteBook = (req, res, next) => {
 };
 
 exports.getAllBooks = (req, res, next) => {
-    Book.find().then(
-      (books) => {
-        res.status(200).json(books);
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-}
+  Book.find().then(
+    (books) => {
+      res.status(200).json(books);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+  next();
+};
+
+exports.bestRating = (req, res, next) => {
+  next();
+};
+
+exports.singleRating = (req, res, next) => {
+  next();
+};
+
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -8,8 +8,10 @@ const bookCtrl = require('../controllers/book');
 
 router.post('/', auth, multer, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
+router.get('/' , bookCtrl.getAllBooks);
+router.get('/bestrating', bookCtrl.bestRating);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, multer, bookCtrl.deleteBook);
-router.get('/' , bookCtrl.getAllBooks);
+router.post('/:id/rating', auth, multer, bookCtrl.singleRating);
 
 module.exports = router;
