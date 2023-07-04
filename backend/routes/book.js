f7db9a39c1960f8f6e5e048651d226527d89e579
Bilou <pierrepillot0110@gmail.com>
2023-07-04 22:23:41 +0200

    put ok

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -49,8 +49,33 @@ router.post('/', multer, (req, res, next) => {
 
 router.get('/bestrating');
 
-router.put('/:id');
-router.delete('/:id');
+router.put('/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.delete('/:id', auth, multer, (req, res, next) => {
+  Book.findOne({ _id: req.params.id})
+    .then(book => {
+      console.log('book.userId', book.userId)
+          console.log('req.auth.userId', req.auth.userId)
+        if (book.userId != req.auth.userId) {
+          res.status(401).json({message: 'Not authorized'});
+        } else {
+          const filename = book.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+            Book.deleteOne({_id: req.params.id})
+              .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+              .catch(error => res.status(401).json({ error }));
+          });
+        }
+    })
+    .catch( error => {
+        res.status(500).json({ error });
+    })
+  }
+)
 router.post('/:id/rating');
 
 
