deed02231e48f0c67b271dba842aeb27b04c6d9c
Pierre Pillot <pierrepillot0110@gmail.com>
2023-07-13 12:47:55 +0200

    wip

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const sharp = require("sharp");
+
 
 /*const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -6,14 +8,20 @@ const multer = require('multer');
   'image/png': 'png'
 };*/
 
-const storage = multer.diskStorage({
+
+const storage = multer.memoryStorage({
   destination: (req, file, callback) => {
     callback(null, 'images')
   },
-  filename: (req, file, callback) => {
+  filename: async (req, file, callback) => {
   /*  const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];*/
-    callback(null, Date.now() + '.' + 'webp');
+    const { buffer, originalname } = req.file;
+    const timestamp = new Date().toISOString();
+    const ref = `${timestamp}-${originalname}.webp`;
+    await sharp(buffer)
+      .webp({ quality: 20 })
+      .toFile("./images/" + ref);
   }
 });
 
