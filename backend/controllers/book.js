ab0d3dd4e83f3b16df5dfc2cef8769a9fb474f28
pierr <pierr@LAPTOP-C4OQM8US.local>
2023-07-17 10:25:28 +0200

    modifyBook

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -18,10 +18,25 @@ exports.createBook = async (req, res) => {
         .catch((error) => {res.status(400).json({ error })});
 };
 
-exports.modifyBook = (req, res) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Livre modifié !'}))
-        .catch(error => res.status(400).json({ error }));
+exports.modifyBook = async (req, res) => {
+    const newImg = await compressImg(req.file.buffer);
+    Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            if(book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Non autorisé !' });
+            } else {
+                const filename = book.imageUrl;
+                fs.unlink(`images/${filename}`, () => {                 
+                    Book.updateOne({ _id: req.params.id }, { ...req.body, imageUrl: newImg, _id: req.params.id })
+                        .then(() => fs.unlink(`images/${actualImg}`))
+                        .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+                        .catch(error => res.status(400).json({ error }));
+                })
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ error });
+        });
 };
 
 exports.getOneBook = (req, res) => {
